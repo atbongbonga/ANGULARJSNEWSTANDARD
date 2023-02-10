b19e2f91f3e4586cd6b6890ed80f4387594a3379
atbongbonga <alvenbongbonga03@gmail.com>
2023-02-10 11:29:48 +0800

    core

---
+++ Bookkeeping/Bookkeeping.Library/Auth/AuthRepository.cs
@@ -0,0 +1,32 @@
+﻿using AccountingLegacy;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookkeeping.Library.Auth
+{
+    internal class AuthRepository
+    {
+        private readonly SERVER server;
+
+        public AuthRepository()
+        {
+            server = new SERVER("DISBURSEMENTS");
+        }
+
+        public EmployeeModel Login(string username, string password)
+        {
+            using (IDbConnection cn = new SqlConnection(server.SAP_HPCOMMON))
+            {
+                var storedProc = "UserLogin";
+                var parameters = new { UserName = username, Password = password };
+                return cn.QueryFirst<EmployeeModel>(storedProc, parameters, commandType: CommandType.StoredProcedure, commandTimeout: 0);
+            }
+        }
+    }
+}
---
+++ Disbursements/Disbursements.Api/Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard authorization header using the Bearer scheme (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(b => b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(x => x.SwaggerEndpoint("v1/swagger.json", "My API V1"));
+}
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
