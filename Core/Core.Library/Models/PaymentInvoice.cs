171ac7abbadca5c69902487199e8c91d4209070e
Christopher Bermudez <chrisbermudez@hpretiro.hi-precision.com.ph>
2023-02-16 14:52:54 +0800

    Added PaymentInvoice(VPM4) Model to Core

---
+++ Core/Core.Library/Models/PaymentInvoice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Library.Models
+{
+    public class PaymentInvoice
+    {
+        public int DocNum { get; set; }
+        public int InvoiceId { get; set; }
+        public int DocEntry { get; set; }
+        public int InvType { get; set; }
+        public decimal SumApplied { get; set; }
+        public int DocTransId { get; set; }
+
+    }
+}
