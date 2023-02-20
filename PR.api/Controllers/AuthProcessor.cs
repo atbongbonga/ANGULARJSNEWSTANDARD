df50005d043200b9259dd646ca27e8183e05b83f
Freddierick Santos <vladimer.quintia@mail.com>
2023-02-20 09:28:19 +0800

    PR init

---
+++ PR.api/Controllers/AuthProcessor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PR.api.Controllers
+{
+    public class AuthProcessor : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
