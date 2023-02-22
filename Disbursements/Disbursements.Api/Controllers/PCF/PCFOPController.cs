6b2982bd8c9937ef66a870287d02710dab5a3af6
CARL QUINTANA <carlquintana20@gmail.com>
2023-02-22 10:47:16 +0800

    CMQ PCF - Other Payments

---
+++ Disbursements/Disbursements.Api/Controllers/PCF/PCFOPController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AccountingLegacy.Disbursements.Library.PCF.ViewModels;
+using AccountingLegacy.Disbursements.Library.PCF.Services;
+
+namespace Disbursements.Api.Controllers.PCF
+{
+    [Route("api/pcfop")]
+    [ApiController]
+    public class PCFOPController : ControllerBase
+    {
+
+        public ActionResult PostPCFOP([FromBody] PCFOPView model) {
+            try
+            {
+                PCFOPService service = new PCFOPService();
+                return Ok(service.PostPCFOP(model));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.GetBaseException().Message);
+            }
+        }
+    }
+
+}
---
+++ Disbursements/Disbursements.Library/Interfaces/Repositories/IPCFOPrepository.cs
@@ -0,0 +1,16 @@
+﻿using AccountingLegacy.Disbursements.Library.PCF.Models;
+using AccountingLegacy.Disbursements.Library.PCF.ViewModels;
+using AccountingLegacy.Disbursements.Library.COPS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingLegacy.Disbursements.Library.Interfaces.Repositories
+{
+    public interface IPCFOPrepository
+    {
+        int PostPCFOP(OPPostingModel model);
+    }
+}
