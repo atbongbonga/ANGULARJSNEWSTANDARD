3929478aab1b74514a7592b632945d998f8b5eda
Christopher Bermudez <chrisbermudez@hpretiro.hi-precision.com.ph>
2023-02-22 15:05:30 +0800

    commit to master

--- Core/Core.Library/SERVER.cs
+++ Core/Core.Library/SERVER.cs
@@ -11,6 +11,7 @@ namespace AccountingLegacy
         private readonly string appName;
         private readonly string SAP_HPCOMMON_ = string.Empty;
         private readonly string SAP_DISBURSEMENTS_ = string.Empty;
+        private readonly string SAP_PF_ = string.Empty;
         private readonly string EMS_HPCOMMON_ = string.Empty;
         public SERVER(string ApplicationName)
         {
@@ -18,9 +19,12 @@ namespace AccountingLegacy
             SAP_HPCOMMON_ = $"Data Source=172.30.0.17;Initial Catalog=HPCOMMON;Integrated Security=False;UID=sapdb;PWD=sapdb; Application Name={ appName }";
             SAP_DISBURSEMENTS_ = $"Data Source=172.30.0.17;Initial Catalog=DISBURSEMENTS;Integrated Security=False;UID=sapdb;PWD=sapdb; Application Name={ appName }";
             EMS_HPCOMMON_ = $"Data Source=192.171.10.51;Initial Catalog=HPCOMMON;Integrated Security=False;UID=sapdb;PWD=sapdb; Application Name={ appName }";
+            EMS_HPCOMMON_ = $"Data Source=192.171.10.51;Initial Catalog=HPCOMMON;Integrated Security=False;UID=sapdb;PWD=sapdb; Application Name={ appName }";
+            SAP_PF_ = $"Data Source=192.171.10.51;Initial Catalog=PF;Integrated Security=False;UID=sapdb;PWD=sapdb; Application Name={ appName }";
         }
 
         public string SAP_HPCOMMON => SAP_HPCOMMON_;
+        public string SAP_PF => SAP_PF_;
         public string EMS_HPCOMMON => EMS_HPCOMMON_;
         public string SAP_DISBURSEMENTS => SAP_DISBURSEMENTS_;
     }
---
+++ PF/PF.Library/Models/BankArrErrorLogs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PF.Library.Models
+{
+    public class BankArrErrorLogs
+    {
+        public int ErrorId { get; set; }
+        public DateTime ErrorDate { get; set; }
+        public string ErrorMsg { get; set; }
+        public string Module { get; set; }
+        public int DocEntry { get; set; }
+        public string Remarks { get; set; }
+        public string PostedBy { get; set; }
+    }
+}
