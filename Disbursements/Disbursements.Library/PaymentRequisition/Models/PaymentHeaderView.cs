6d037afeb7b79d687b8e0cf629acd489d1bbbf2f
Christopher Bermudez <chrisbermudez@hpretiro.hi-precision.com.ph>
2023-03-01 09:19:32 +0800

    commit to check PCF

---
+++ Disbursements/Disbursements.Library/PaymentRequisition/Models/PaymentHeaderView.cs
@@ -0,0 +1,13 @@
+﻿using Core.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disbursements.Library.PaymentRequisition.Models
+{
+    public class PaymentHeaderView : Payment
+    {
+    }
+}
---
+++ Disbursements/Disbursements.Library/PaymentRequisition/Repositories/PaymentRepository.cs
@@ -0,0 +1,42 @@
+﻿using AccountingLegacy.Core.Library;
+using Disbursements.Library.COPS.ViewModels.JGMAN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disbursements.Library.PaymentRequisition.Repositories
+{
+    internal class PaymentRepository
+    {
+        private readonly string userCode;
+        public PaymentRepository(string userCode = "")
+        {
+            this.userCode = userCode;
+        }
+
+        public void PostPayment(PaymentView payment)
+        {
+            try
+            {
+                using (var sap = new SAPBusinessOne("172.30.1.167"))
+                {
+                    var pay = sap.VendorPayments;
+                    if (pay.Add() == 0)
+                    {
+
+                    } else
+                    {
+                        throw new ApplicationException(sap.Company.GetLastErrorDescription());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //ERROR LOGS
+                throw ex;
+            }
+        }
+    }
+}
